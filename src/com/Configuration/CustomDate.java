c290e8a2f8d985b15be3f257e386a2ec13e02fb6
deepashah25 <37476678+deepashah25@users.noreply.github.com>
2022-01-19 21:33:44 +0530

    Update CustomDate.java

--- src/com/Configuration/CustomDate.java
+++ src/com/Configuration/CustomDate.java
@@ -10,12 +10,11 @@ import java.util.Scanner;
 public class CustomDate {
 public static void main(String[] args) {
 	Scanner sc=new Scanner(System.in);
-	/*
+	
 	 Date d1=new Date(); System.out.println(d1); 
 	 LocalDate l1=LocalDate.now();
 	 System.out.println(l1);
-	 */
-	//
+	
 	int i=Integer.parseInt("78");
 	
 	System.out.println("Enter your birthdate dd/mm/yyyy");
